9211778112da275c120a4608046ea1f59c0fd6c0
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-05 03:08:15 -0600

    register validation

--- apps/web/src/routes/register/+page.server.js
+++ apps/web/src/routes/register/+page.server.js
@@ -1,15 +1,23 @@
-import { error, redirect } from "@sveltejs/kit"
-import { generateUsername } from "../../lib/utils"
+import { error, fail, redirect } from "@sveltejs/kit"
+import { generateUsername, validateData } from "$lib/utils"
+import { registerUserSchema } from "$lib/schemas"
 
 export const actions = {
   register: async ({ locals, request }) => {
-    const body = Object.fromEntries(await request.formData())
+    const { formData, errors } = await validateData(await request.formData(), registerUserSchema)
 
-    let username = generateUsername(body.name.split(' ').join('')).toLowerCase()
+    if (errors) {
+      return fail(400, {
+        data: formData,
+        errors: errors.fieldErrors
+      })
+    }
+
+    let username = generateUsername(formData.name.split(' ').join('')).toLowerCase()
 
     try {
-      await locals.pb.collection('users').create({ username, ...body })
-      await locals.pb.collection('users').requestVerification(body.email)
+      await locals.pb.collection('users').create({ username, ...formData })
+      await locals.pb.collection('users').requestVerification(formData.email)
     } catch (err) {
       console.log('Error:', err)
       throw error(500, 'Something went wrong')
