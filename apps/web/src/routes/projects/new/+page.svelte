c54e2087682c72fd4738b5948be3422b3ac7100e
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-04 19:41:31 -0600

    modified:   apps/web/src/routes/projects/new/+page.svelte

---
+++ apps/web/src/routes/projects/new/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Input } from '$lib/components';
+</script>
+
+<div class="flex flex-col w-full h-full p-2">
+	<div class="w-full">
+		<form
+			action="?/create"
+			method="POST"
+			class="flex flex-col space-y-2 w-full items-center"
+			enctype="multipart/form-data"
+		/>
+	</div>
+</div>
