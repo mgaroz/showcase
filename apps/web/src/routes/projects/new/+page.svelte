cf001cbe22c228e5a71a3ce01e22d1e2001da063
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-05 04:11:07 -0600

    new project: validation

--- apps/web/src/routes/projects/new/+page.svelte
+++ apps/web/src/routes/projects/new/+page.svelte
@@ -16,21 +16,21 @@
 		>
 			<h3 class="text-3xl font-bold">Tells us more about this project</h3>
 			<p class="mt-2 text-lg">We'll need the name, tagline, link and description</p>
-			<Input id="name" label="Project name" value={form?.data?.name} />
-			<Input id="tagline" label="Project tagline" value={form?.data?.tagline} />
-			<Input id="url" label="Project URL" value={form?.data?.url} />
-			<TextArea id="description" label="Project description" value={form?.data?.description} />
-			<div class="form-control w-full max-w-lg">
-				<label for="thumbnail" class="label font-medium pb-1">
-					<span class="label-text">Thumbnail</span>
-				</label>
-				<input
-					type="file"
-					name="thumbnail"
-					id="thumbnail"
-					class="file-input file-input-bordered w-full max-w-lg"
-				/>
-			</div>
+			<Input id="name" label="Project name" value={form?.data?.name} errors={form?.errors?.name} />
+			<Input
+				id="tagline"
+				label="Project tagline"
+				value={form?.data?.tagline}
+				errors={form?.errors?.tagline}
+			/>
+			<Input id="url" label="Project URL" value={form?.data?.url} errors={form?.errors?.url} />
+			<TextArea
+				id="description"
+				label="Project description"
+				value={form?.data?.description}
+				errors={form?.errors?.description}
+			/>
+			<Input id="thumbnail" label="Thumbnail" type="file" errors={form?.errors?.thumbnail} />
 			<button type="submit" class="btn btn-primary w-full max-w-lg">Create Project</button>
 		</form>
 	</div>
