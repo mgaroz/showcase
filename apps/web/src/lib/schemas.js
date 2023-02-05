a98095f1cc0b52a98052b06b15d16070bf5381f7
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-05 03:38:38 -0600

    formatting

--- apps/web/src/lib/schemas.js
+++ apps/web/src/lib/schemas.js
@@ -1,15 +1,28 @@
 import { z } from 'zod'
 
 export const loginUserSchema = z.object({
-  email: z.string({ required_error: 'Email is required' }).email({ message: 'Email must be a valid email.' }),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email({ message: 'Email must be a valid email.' }),
   password: z.string({ required_error: 'Password is required' })
 })
 
 export const registerUserSchema = z.object({
-  name: z.string({ required_error: 'Name is required' }).regex(/^[a-zA-Z\s]*$/, { message: 'Name can only contain letters and spaces' }).min(2, { message: 'Name must be at least 2 characters long' }).max(64, { message: 'Name must be less than 64 characters long' }).trim(),
-  email: z.string({ required_error: 'Email is required' }).email({ message: 'Email must be a valid email' }),
-  password: z.string({ required_error: 'Password is required.' }).regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, { message: 'Password must be at least 8 characters long & contain at least  one letter, one digit and one special character' }),
-  passwordConfirm: z.string({ required_error: 'Confirm Password is required.' }).regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, { message: 'Password must be at least 8 characters long & contain at least  one letter, one digit and one special character' })
+  name: z
+    .string({ required_error: 'Name is required' })
+    .regex(/^[a-zA-Z\s]*$/, { message: 'Name can only contain letters and spaces' })
+    .min(2, { message: 'Name must be at least 2 characters long' })
+    .max(64, { message: 'Name must be less than 64 characters long' })
+    .trim(),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email({ message: 'Email must be a valid email' }),
+  password: z
+    .string({ required_error: 'Password is required.' })
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, { message: 'Password must be at least 8 characters long & contain at least one letter, one digit and one special character' }),
+  passwordConfirm: z
+    .string({ required_error: 'Confirm Password is required.' })
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, { message: 'Password must be at least 8 characters long & contain at least one letter, one digit and one special character' })
 }).superRefine(({ passwordConfirm, password }, ctx) => {
   if (passwordConfirm !== password) {
     ctx.addIssue({
@@ -23,4 +36,25 @@ export const registerUserSchema = z.object({
       path: ['passwordConfirm']
     })
   }
+})
+
+export const createProjectSchema = z.object({
+  name: z
+    .string({ required_error: 'Name is required' })
+    .min(1, { message: 'Name is required' })
+    .max(64, { message: 'Name cannot exceed 64 characters' })
+    .trim(),
+  tagline: z
+    .string({ required_error: 'Tagline is required' })
+    .min(1, { message: 'Tagline is required' })
+    .max(64, { message: 'Tagline cannot exceed 64 characters' })
+    .trim(),
+  url: z
+    .string({ required_error: 'URL is required' })
+    .url({ message: 'URL must be a valid URL' }),
+  description: z
+    .string({ required_error: 'Description is required' })
+    .min(1, { message: 'Description is required' })
+    .max(512, { message: 'Description cannot exceed 512 characters' })
+    .trim()
 })
