59bfa502f8b75232ffb10cefc60956bc40c8fb3d
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-05 02:13:03 -0600

    implemented login error messages

--- apps/web/src/lib/components/Input.svelte
+++ apps/web/src/lib/components/Input.svelte
@@ -6,6 +6,8 @@
 	export let type = 'text';
 	export let disabled = false;
 	export let required = false;
+
+	export let errors;
 </script>
 
 <div class="form-control w-full max-w-lg mb-2">
@@ -22,4 +24,13 @@
 		name={id}
 		{value}
 	/>
+	{#if errors}
+		${#each errors as error}
+			<label for={id} class="label py-0 pt-1">
+				<span class="label-text-alt text-error">
+					{error}
+				</span>
+			</label>
+		{/each}
+	{/if}
 </div>
