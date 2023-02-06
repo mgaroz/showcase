d74e90576d6749a6dd2d33072591525eefce8ef2
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-05 18:51:18 -0600

    password update validation

--- apps/web/src/routes/my/settings/security/+page.server.js
+++ apps/web/src/routes/my/settings/security/+page.server.js
@@ -1,13 +1,21 @@
-import { error, redirect } from '@sveltejs/kit'
+import { error, redirect, fail } from '@sveltejs/kit'
+import { validateData } from '$lib/utils'
+import { updatePasswordSchema } from '$lib/schemas'
 
 export const actions = {
   updatePassword: async ({ request, locals }) => {
-    const data = Object.fromEntries(await request.formData())
+    const { formData, errors } = await validateData(await request.formData(), updatePasswordSchema)
+
+    if (errors) {
+      return fail(400, {
+        errors: errors.fieldErrors
+      })
+    }
 
     try {
-      await locals.pb.collection('users').update(locals.user.id, data)
+      await locals.pb.collection('users').update(locals.user.id, formData)
       locals.pb.authStore.clear()
-    } catch (error) {
+    } catch (err) {
       console.log('Error: ', err)
       throw error(err.status, err.message)
     }
