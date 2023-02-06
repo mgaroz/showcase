0efacded0877b90d05ed73f689c88a72950aac72
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-05 20:02:33 -0600

    login toasts

--- apps/web/src/routes/login/+page.server.js
+++ apps/web/src/routes/login/+page.server.js
@@ -23,7 +23,7 @@ export const actions = {
       }
     } catch (err) {
       console.log('Error: ', err)
-      throw error(500, 'Something went wrong logging in')
+      throw error(err.status, err.message)
     }
     throw redirect(303, '/')
   }
