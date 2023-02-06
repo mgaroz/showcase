7b31224fbf344df1925981c14099fc12c31e5b64
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-05 20:40:34 -0600

    increased grid gap

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-col gap-3">
 	<h1 class="text-xl font-bold">Recent Projects</h1>
-	<div class="grid grid-cols-3 gap-2">
+	<div class="grid grid-cols-3 gap-8">
 		{#each data.projects as project}
 			<ProjectCard {project} />
 		{/each}
