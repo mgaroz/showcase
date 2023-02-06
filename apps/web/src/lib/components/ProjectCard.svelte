9fec9d9c5ec64aeb0cd5d5d3f94f35846ca4e9f3
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-05 20:56:40 -0600

    image scale on hover

--- apps/web/src/lib/components/ProjectCard.svelte
+++ apps/web/src/lib/components/ProjectCard.svelte
@@ -3,13 +3,14 @@
 	export let project;
 </script>
 
-<div class="card w-96 bg-base-100 shadow-xl">
+<div class="card w-96 bg-base-100 shadow-xl group">
 	<figure>
 		<img
 			src={project.thumbnail
 				? getImageURL(project.collectionId, project.id, project.thumbnail)
 				: `https://via.placeholder.com/300/4506CB/FFFFFF/?text=${project.name}`}
 			alt="project thumbnail"
+			class="transition-transform group-hover:scale-110"
 		/>
 	</figure>
 	<div class="card-body">
